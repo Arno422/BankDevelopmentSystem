c105519a7a810f8fde9ee41a5d6c9e454e1a9b79
Arno422 <2025283@student.cct.ie>
2026-05-18 23:11:10 +0100

    created classes

---
+++ BankDevelopment/src/main/java/CA_2/Department.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author aramn
+ */
+public class Department {
+    
+    private String departmentName;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+}
