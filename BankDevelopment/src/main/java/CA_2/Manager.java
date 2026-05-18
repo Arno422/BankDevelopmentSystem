55270170ce818cc607b1280cee09be3628f94e24
Arno422 <2025283@student.cct.ie>
2026-05-18 23:27:08 +0100

    manager class created and added manager types

---
+++ BankDevelopment/src/main/java/CA_2/Manager.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author aramn
+ */
+public class Manager {
+
+    private String managerType;
+
+    public Manager(String managerType) {
+        this.managerType = managerType;
+    }
+
+    public String getManagerType() {
+        return managerType;
+    }
+}
+
