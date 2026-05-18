c6ba8cc5b0aad62198152d87717662c5d2f4c01b
Arno422 <2025283@student.cct.ie>
2026-05-18 23:44:38 +0100

    Menu option used enum

---
+++ BankDevelopment/src/main/java/CA_2/MenuOption.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author aramn
+ */
+public enum MenuOption {
+    // Menu option for sorting employees
+    SORT,
+    // Menu option for searching employee
+    SEARCH,
+
+    // Menu option for adding new employee
+    ADD_RECORD,
+
+    // Menu option for creating binary tree
+    CREATE_TREE,
+
+    // Menu option for displaying all employees
+    DISPLAY_ALL,
+
+    // Menu option for exiting program
+    EXIT
+}
