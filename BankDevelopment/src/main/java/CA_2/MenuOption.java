0da56df838be5a10275842bf8554c2d2fad8fde4
Arno422 <2025283@student.cct.ie>
2026-05-23 12:05:38 +0100

    ENUM menu option added

--- BankDevelopment/src/main/java/CA_2/MenuOption.java
+++ BankDevelopment/src/main/java/CA_2/MenuOption.java
@@ -4,25 +4,45 @@
  */
 package CA_2;
 
-/**
- *
- * @author aramn
- */
+
 public enum MenuOption {
     // Menu option for sorting employees
-    SORT,
+    SORT(1),
     // Menu option for searching employee
-    SEARCH,
+    SEARCH(2),
 
     // Menu option for adding new employee
-    ADD_RECORD,
+    ADD_RECORD(3),
 
     // Menu option for creating binary tree
-    CREATE_TREE,
+    CREATE_TREE(4),
 
     // Menu option for displaying all employees
-    DISPLAY_ALL,
+    DISPLAY_ALL(5),
 
     // Menu option for exiting program
-    EXIT
+    EXIT(6);
+    
+    private int valueMenuOption(int value) {
+        this.value = value;
+    }
+
+    // getter
+    public int getValue() {
+        return value;
+    }
+
+    // convert int → enum
+    public static MenuOption fromInt(int number) {
+
+        for (MenuOption option : MenuOption.values()) {
+
+            if (option.value == number) {
+                return option;
+            }
+        }
+
+        return null;
+    }
 }
+
